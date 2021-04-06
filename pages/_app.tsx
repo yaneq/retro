0d1fce0f64d8bff24bc0cedfdaf68155ab69491c
Jan Koeppen <jan@yaneq.com>
2021-04-06 16:54:52 -0300

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import { FirebaseProvider } from "@providers"
 import "bootstrap/dist/css/bootstrap.css"
+import "tailwindcss/tailwind.css"
 import React from "react"
 import "../styles/paper-kit.css"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
   return (
