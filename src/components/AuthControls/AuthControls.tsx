555c75f55e30aacf494e11705fd0267645f6eff1
Jan Koeppen <jan@yaneq.com>
2021-04-06 16:59:19 -0300

    redirect on logout

--- src/components/AuthControls/AuthControls.tsx
+++ src/components/AuthControls/AuthControls.tsx
@@ -4,10 +4,12 @@ import { Button } from "react-bootstrap"
 import firebase from "firebase/app"
 import "firebase/auth"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export const AuthControls = () => {
   const auth = useAuth()
   const { user, loading } = useUser()
+  const router = useRouter()
 
   const googleLogin = () => {
     let provider = new firebase.auth.GoogleAuthProvider()
@@ -18,6 +20,7 @@ export const AuthControls = () => {
 
   const logOut = () => {
     auth.signOut()
+    router.push("/")
   }
 
   return (
@@ -29,12 +32,17 @@ export const AuthControls = () => {
       )}
       {user && (
         <>
-          <Link href="/boards">
-            <Button>Go to dashboard</Button>
-          </Link>
-          <Button variant={"link"} onClick={logOut}>
+          <a
+            className="mr-5 text-sm font-semibold text-gray-600 hover:text-gray-800 cursor-pointer"
+            onClick={logOut}
+          >
             Log out
-          </Button>
+          </a>
+          <Link href="/boards">
+            <Button className="items-center px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg hover:bg-blueGray-900 focus:ring focus:outline-none">
+              Dashboard
+            </Button>
+          </Link>
         </>
       )}
     </>
