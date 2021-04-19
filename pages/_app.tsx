03ddaa9420f891314c84d374174528719f880a4c
Jan Koeppen <jan@yaneq.com>
2021-04-18 23:52:17 -0300

    style cleanups

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
-import { FirebaseProvider } from "@providers"
-import "../styles/globals.css"
-import "bootstrap/dist/css/bootstrap.css"
-import "tailwindcss/tailwind.css"
 import React from "react"
-import "../styles/paper-kit.css"
+import { FirebaseProvider } from "@providers"
+import Head from "next/head"
+import "../styles/globals.css"
+// import "bootstrap/dist/css/bootstrap.css"
+import "tailwindcss/tailwind.css"
+
+// import "../styles/paper-kit.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <FirebaseProvider>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
       <Component {...pageProps} />
     </FirebaseProvider>
   )
