80d68bb0f1121a3210cb4fa009d8732a848261ed
Jan Koeppen <jan@yaneq.com>
2021-04-23 10:58:24 -0300

    change button style on board list

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -59,7 +59,7 @@ export const Board = forwardRef(({ board }: { board: iBoard }, ref) => {
         <Link href={`/boards/${board.id}`} key={board.id}>
           <a
             href="/boards/"
-            className="bg-black text-white font-bold py-2 px-10 rounded-lg text-grey-800"
+            className="items-center px-10 py-2 mt-10 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg hover:bg-green-500 focus:outline-none bg-green-400 cursor-pointer"
           >
             Open
           </a>
