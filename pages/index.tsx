69c99d9b5cdc89412bc2ed61a4b3dbc820faf866
Jan Koeppen <jan@yaneq.com>
2021-04-27 11:09:09 -0300

    rounded video corners

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const Landing = () => {
           Retrospectives <span className="text-primary-btn">you will love</span>
         </p>
         <div className="bg-green-400 rounded-2xl shadow-3xl p-5 sm:p-20 my-10 text-white font-sans">
-          <video controls autoPlay loop muted>
+          <video controls autoPlay loop muted className={"rounded-xl"}>
             <source src="/reretro-preview.webm" type="video/webm" />
           </video>
 
