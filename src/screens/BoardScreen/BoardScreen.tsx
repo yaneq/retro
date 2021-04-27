25d19979cc15fb114ae6a3690705b2a3af994134
Jan Koeppen <jan@yaneq.com>
2021-04-26 21:58:39 -0300

    add top menu to boardscreen

--- src/screens/BoardScreen/BoardScreen.tsx
+++ src/screens/BoardScreen/BoardScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from "react"
-import { RadioSlide } from "@components"
+import { RadioSlide, TopMenu } from "@components"
 import { iBoard, iCard } from "@types"
 import { useAuth, useFirebase, useUser } from "@providers"
 import { useRouter } from "next/router"
@@ -45,55 +45,58 @@ export const BoardScreen = () => {
   const quote = boardId && getRandomQuote(boardId)
 
   return (
-    <div className="container mx-auto">
-      <div className="text-3xl py-3  font-sans font-bold text-gray-700 h-16">
-        {user && user?.uid === board?.createdBy && (
-          <Link href={`/boards`}>
-            <a className="pr-3 py-2">
-              <FaChevronLeft size={15} style={{ display: "inline" }} />
-            </a>
-          </Link>
-        )}
-        <span className="font-serif">{board?.title}</span>
-      </div>
+    <>
+      <TopMenu showAuth={true} />
+      <div className="container mx-auto">
+        <div className="text-3xl py-3  font-sans font-bold text-gray-700 h-16">
+          {user && user?.uid === board?.createdBy && (
+            <Link href={`/boards`}>
+              <a className="pr-3 py-2">
+                <FaChevronLeft size={15} style={{ display: "inline" }} />
+              </a>
+            </Link>
+          )}
+          <span className="font-serif">{board?.title}</span>
+        </div>
 
-      <div className="hidden sm:block">
-        <RadioSlide
-          options={BOARD_STAGES}
-          currentValue={board?.stage}
-          onChangeCallback={(newValue) =>
-            boardDomain.updateBoard(board, { stage: newValue })
-          }
-        />
-      </div>
-      <div className="block sm:hidden text-lg font-sans p-4 bg-gray-100 rounded-xl text-center">
-        <p className={"font-mono font text-xs"}>current step:</p>
-        <p className="font-bold">{board?.stage}</p>
-      </div>
-
-      <div className="my-5">
-        {board && board?.stage === "prepare" && (
-          <div className={"sm:m-20 rounded-xl bg-gray-100 p-10"}>
-            <p
-              className={
-                "text-xl pb-4 flex font-serif italic leading-relaxed text-gray-500 whitespace-pre-line"
-              }
-            >
-              {quote.quote}
-            </p>
-            <p className={"text-md font-bold text-right"}>{quote.source}</p>
-          </div>
-        )}
-
-        {board && board?.stage !== "prepare" && (
-          <BoardColumns
-            board={board}
-            cards={cards}
-            user={user}
-            boardDomain={boardDomain}
+        <div className="hidden sm:block">
+          <RadioSlide
+            options={BOARD_STAGES}
+            currentValue={board?.stage}
+            onChangeCallback={(newValue) =>
+              boardDomain.updateBoard(board, { stage: newValue })
+            }
           />
-        )}
+        </div>
+        <div className="block sm:hidden text-lg font-sans p-4 bg-gray-100 rounded-xl text-center">
+          <p className={"font-mono font text-xs"}>current step:</p>
+          <p className="font-bold">{board?.stage}</p>
+        </div>
+
+        <div className="my-5">
+          {board && board?.stage === "prepare" && (
+            <div className={"sm:m-20 rounded-xl bg-gray-100 p-10"}>
+              <p
+                className={
+                  "text-xl pb-4 flex font-serif italic leading-relaxed text-gray-500 whitespace-pre-line"
+                }
+              >
+                {quote.quote}
+              </p>
+              <p className={"text-md font-bold text-right"}>{quote.source}</p>
+            </div>
+          )}
+
+          {board && board?.stage !== "prepare" && (
+            <BoardColumns
+              board={board}
+              cards={cards}
+              user={user}
+              boardDomain={boardDomain}
+            />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
